d9afd30fa6ae599778491e9214c496bf41b0edc8
AndanteDevs <orrdaniel73@gmail.com>
2019-09-06 22:20:44 +0100

    v0.1.0

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    void Update()
+    {
+        if ( Input.GetButton("Cancel") )
+        {
+            ResetToMenu();
+        }
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Starting game...");
+        LoadNextScene();
+        Debug.Log("Game started");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+        Debug.Log("Game quit");
+    }
+
+    void LoadNextScene()
+    {
+        Debug.Log("Loading next scene in sequence...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void ResetToMenu()
+    {
+        Debug.Log("Unloading " + SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("menu");
+    }
+
+    public void ResetScene()
+    {
+        Debug.Log("Resetting " + SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log(SceneManager.GetActiveScene().name + " reset");
+    }
+
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public Transform playerTransform;
+    public PlayerMovement playerMovement;
+    public GameObject DeathPanel;
+    public float playerYMin;
+    public float deathResetDelay = 1;
+    public bool onGround = false;
+    bool playerHasDied = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if ( playerTransform.position.y < playerYMin )
+        {
+            if ( playerHasDied == false )
+            {
+                Debug.Log("Player death");
+                playerHasDied = true;
+                playerMovement.enabled = false;
+                DeathPanel.SetActive(true);
+                Invoke("PlayerDeath", deathResetDelay);
+            }
+        }
+    }
+
+    void PlayerDeath()
+    {
+        FindObjectOfType<GameManager>().ResetScene();
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if ( collisionInfo.collider.tag == "Ground" )
+        {
+            onGround = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        if ( collisionInfo.collider.tag == "Ground" )
+        {
+            onGround = false;
+        }
+    }
+}
