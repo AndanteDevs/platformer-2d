40c052ec30c82813689373e8a7fa8dd9d596117e
AndanteDevs <orrdaniel73@gmail.com>
2019-09-08 14:42:10 +0100

    v0.3.0-pre2
    Fixed movement
    Fixed sticking to sides of platforms

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    public float movementSpeed = .85f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+
+        if ( Input.GetButton("Jump") )
+        {
+            jump = true;
+        }
+
+        if ( Input.GetButtonDown("Fire3") )
+        {
+            crouch = true;
+        } else if ( Input.GetButtonUp("Fire3") )
+        {
+            crouch = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove, crouch, jump);
+        jump = false;
+    }
+}
